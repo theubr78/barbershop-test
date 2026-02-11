1c6390e570403928633354fc83133bc786907619
theubr78 <matheusdesantana05@gmail.com>
2026-02-11 01:53:11 -0300

    chore: debug node version in simple-test

--- netlify/functions/simple-test.cjs
+++ netlify/functions/simple-test.cjs
@@ -1,6 +1,11 @@
 exports.handler = async function (event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello from CJS" })
+        body: JSON.stringify({
+            message: "Hello from CJS",
+            nodeVersion: process.version,
+            hasFetch: typeof fetch !== 'undefined',
+            envTest: process.env.NODE_VERSION
+        })
     };
 };
