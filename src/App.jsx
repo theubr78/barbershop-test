b89fe0854ca356e39dbc741d95f3e3cd4c202f80
theubr78 <matheusdesantana05@gmail.com>
2026-02-11 01:16:10 -0300

    fix: import Navigate in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import SetupAdmin from './SetupAdmin'
 import { AuthProvider } from './contexts/AuthContext'
 import { BarbershopProvider } from './contexts/BarbershopContext'
