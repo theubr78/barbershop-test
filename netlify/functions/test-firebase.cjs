d750490a6b1416347a8e65b57801d35ccc676644
theubr78 <matheusdesantana05@gmail.com>
2026-02-11 01:53:57 -0300

    chore: test firestore subpath import

--- netlify/functions/test-firebase.cjs
+++ netlify/functions/test-firebase.cjs
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin');
+const { getFirestore } = require('firebase-admin/firestore');
 
 exports.handler = async function (event, context) {
     return {
         statusCode: 200,
         body: JSON.stringify({
             message: "Firebase loaded",
-            apps: admin.apps ? admin.apps.length : 0
+            apps: admin.apps ? admin.apps.length : 0,
+            hasFirestore: typeof getFirestore === 'function'
         })
     };
 };
